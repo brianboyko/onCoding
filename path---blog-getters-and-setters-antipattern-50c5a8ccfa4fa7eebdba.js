2cb4ebeb7a0ac2c50617ca4717f9c1ef29f0039e
Brian Boyko <brian.boyko@gmail.com>
2018-06-23 00:18:50 -0400

    Updates

---
+++ path---blog-getters-and-setters-antipattern-50c5a8ccfa4fa7eebdba.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xf6eba88b15e1],{412:function(t,e){t.exports={data:{markdownRemark:{html:"<p>Hey, you love </p>",frontmatter:{date:"22 June, 2018",path:"/blog/getters-and-setters-antipattern",title:"Getters and Setters: A Javascript antipattern?",tags:["react","vue","observables","getters and setters","es6","typescript","javascript"],featuredImage:null}}},pathContext:{}}}});
+//# sourceMappingURL=path---blog-getters-and-setters-antipattern-50c5a8ccfa4fa7eebdba.js.map
